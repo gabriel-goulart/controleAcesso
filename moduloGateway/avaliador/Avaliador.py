b62d37a900aa39c0cbc4afbff149e7b23ce35bc3
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-20 02:40:58 -0300

    criando classes dos outros módulos

---
+++ moduloGateway/avaliador/Avaliador.py
@@ -0,0 +1,10 @@
+import Gateway
+
+class Avaliador:
+    
+
+    def __init__(self, gateway):
+        self.gateway = gateway
+
+    def avaliar(self, contextoAtual, contextoRegra):
+        return 0   
---
+++ moduloGateway/persistencia/Persistencia.py
@@ -0,0 +1,9 @@
+import Gateway
+
+class Persistencia:
+
+    def __init__(self, gateway):
+        self.gateway = gateway
+
+    def getInformacoesAcesso(self, usuario, ambiente):
+        return 0    
