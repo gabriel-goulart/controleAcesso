b04dce5a26872a2ce792865c986ae4e478f81661
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-24 01:56:50 -0300

    correcao de texto

--- moduloGateway/persistencia/Persistencia.py
+++ moduloGateway/persistencia/Persistencia.py
@@ -19,12 +19,11 @@ class Persistencia:
         retorno['ambiente'] = ambiente
         retorno['usuario'] = usuario
 
-        query = "SELECT pa.id_papel_de_ambiente, pa.nome_papel_de_ambiente, up.id_papel_de_usuario, pu.nome_papel_de_usuario, ra.contexto FROM Regra_De_Acesso ra INNER JOIN Ambiente a on a.id_ambiente = "+ambiente+" INNER JOIN Papel_De_Ambiente pa ON pa.id_papel_de_ambiente = a.id_papel_de_ambiente INNER JOIN Usuario_Papel up ON up.tag_usuario = \""+usuario+"\" AND up.id_papel_de_ambiente = pa.id_papel_de_ambiente INNER JOIN Papel_De_Usuario pu ON pu.id_papel_de_usuario = up.id_papel_de_usuario WHERE ra.id_papel_de_ambiente = pa.id_papel_de_ambiente AND ra.id_papel_de_usuario = pu.id_papel_de_usuario;"        
+        query = "SELECT pa.id_papel_de_ambiente, pa.nome_papel_de_ambiente, up.id_papel_de_usuario, pu.nome_papel_de_usuario, ra.contexto FROM Regra_De_Acesso ra INNER JOIN Ambiente a ON a.id_ambiente = "+ambiente+" INNER JOIN Papel_De_Ambiente pa ON pa.id_papel_de_ambiente = a.id_papel_de_ambiente INNER JOIN Usuario_Papel up ON up.tag_usuario = \""+usuario+"\" AND up.id_papel_de_ambiente = pa.id_papel_de_ambiente INNER JOIN Papel_De_Usuario pu ON pu.id_papel_de_usuario = up.id_papel_de_usuario WHERE ra.id_papel_de_ambiente = pa.id_papel_de_ambiente AND ra.id_papel_de_usuario = pu.id_papel_de_usuario;"        
         try:
-            print("Buscando informações no banco de dados:")            
+            print("##### Buscando informações no banco de dados #####")            
             self.cursor.execute(query)
-            result = self.cursor.fetchone()
-            print(result)
+            result = self.cursor.fetchone()            
             if result == None :
                 retorno = {}
             else:
