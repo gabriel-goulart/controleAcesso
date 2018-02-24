337e266dbcf0a5b0a277ec5d3330710f5bbf560c
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-24 01:58:22 -0300

    chamando a avaliacao de contexto passando as informacoes do ambiente

--- moduloGateway/Gateway.py
+++ moduloGateway/Gateway.py
@@ -1,6 +1,6 @@
-#import Libs
+#import classes
 import json
-
+import time
 # import classe Grenciador
 from gerenciador.Gerenciador import Gerenciador
 # import classe Avaliador
@@ -17,15 +17,25 @@ class Gateway:
 		
 		# sub módulo de persistência
 		self.persistencia = Persistencia(self)
-
+		#infoAcesso = self.persistencia.getInformacoesAcesso("e1be95a5", "2")
+		#print(infoAcesso)
 		# sub módulo de gerenciamento
 		self.gerenciador = Gerenciador(self)
 		self.gerenciador.start()
 
-	def acessoSolicitado(self, info):
+	def tagIdentificada(self, info):
 		infoDeserializada = json.loads(info)
 		#contem todas as informações relacionadas ao acesso (usuario, ambiente, papel usuario, papel ambiente, regra de acesso)
 		informacoesAcesso = self.persistencia.getInformacoesAcesso(infoDeserializada['usuario'], infoDeserializada['ambiente'])
+		
+		# montando o contexto atual
+		contextoAtual = {}
+		contextoAtual['Data'] = time.strftime('%d/%m/%Y')
+		contextoAtual['Tempo'] = time.strftime('%H:%M')
+		contextoAtual['Recurso'] = infoDeserializada['contexto']
+		#print(infoDeserializada['contexto']['3'])
+		resultadoAvaliacao = self.avaliador.avaliar(contextoAtual,informacoesAcesso['contexto'])
+		print("RESULTADO : " + str(resultadoAvaliacao))
 
 	def msg(self):
 		print(" MSG ");	
