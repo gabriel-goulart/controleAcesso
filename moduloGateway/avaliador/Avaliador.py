f7dfd9c11f96effbf482176e707e795961d42984
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-24 01:56:05 -0300

    criado funcao para avaliar os contextos

--- moduloGateway/avaliador/Avaliador.py
+++ moduloGateway/avaliador/Avaliador.py
@@ -1,10 +1,40 @@
+# import classes
+import json
+# import Gateway
 import Gateway
+# import classe de tipo
+from avaliador.Tipo import Tipo
+# import classe de operação
+from avaliador.Operacao import Operacao
 
 class Avaliador:
     
 
     def __init__(self, gateway):
         self.gateway = gateway
-
+        self.tipo = Tipo()
+        self.operacao = Operacao()
+       
     def avaliar(self, contextoAtual, contextoRegra):
-        return 0   
+        print("##### Avaliando contexto #####")
+        print(contextoAtual)
+        print(contextoRegra)
+
+        # verifica se o usuario tem acesso garantido independente de contexto
+        if contextoRegra == None:
+            return True
+        else:
+            avaliacao = False
+            contextoRegra = json.loads(contextoRegra)           
+            for c in contextoRegra:
+                
+                # convertendo os valores de acordo com o seu tipo
+                valorConvertidoRegra,valorConvertidoAtual = getattr(self.tipo,c['Tipo'])(c['Valor'], contextoAtual[c['Tipo']], c['Recurso'])
+                
+                # avaliando os valores de acordo com a operacao
+                avaliacao = getattr(self.operacao,c['Operacao'])(valorConvertidoRegra,valorConvertidoAtual)                
+                print("Avaliação : " + str(avaliacao))
+                if avaliacao == False:
+                    break
+
+            return avaliacao
