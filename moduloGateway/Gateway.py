27c26b5805ddd63e53a78fceef1738618e8d7924
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-27 02:05:39 -0300

    verificacao se o usuario tem sessao e retorno para o ambiente

--- moduloGateway/Gateway.py
+++ moduloGateway/Gateway.py
@@ -24,19 +24,56 @@ class Gateway:
 		self.gerenciador.start()
 
 	def tagIdentificada(self, info):
+		resultadoAvaliacao = False
+		retorno = {}
+		acao = None
+		# deserializando o json que vem do ambiente
 		infoDeserializada = json.loads(info)
-		#contem todas as informações relacionadas ao acesso (usuario, ambiente, papel usuario, papel ambiente, regra de acesso)
-		informacoesAcesso = self.persistencia.getInformacoesAcesso(infoDeserializada['usuario'], infoDeserializada['ambiente'])
-		
-		# montando o contexto atual
-		contextoAtual = {}
-		contextoAtual['Data'] = time.strftime('%d/%m/%Y')
-		contextoAtual['Tempo'] = time.strftime('%H:%M')
-		contextoAtual['Recurso'] = infoDeserializada['contexto']
-		#print(infoDeserializada['contexto']['3'])
-		resultadoAvaliacao = self.avaliador.avaliar(contextoAtual,informacoesAcesso['contexto'])
-		print("RESULTADO : " + str(resultadoAvaliacao))
 
-	def msg(self):
-		print(" MSG ");	
-		self.gerenciador.enviarMensagem('retorno de mensagem')
+		# verificando se o usuario tem uma sessao para o ambiente que esta tentando acessar
+		if self.persistencia.temSessao(infoDeserializada['usuario'], infoDeserializada['ambiente']) == False :
+			print("##### Usuário entrando no ambiente #####")
+			acao = "acessando"
+			# contem todas as informações relacionadas ao acesso (usuario, ambiente, papel usuario, papel ambiente, regra de acesso)
+			informacoesAcesso = self.persistencia.getInformacoesAcesso(infoDeserializada['usuario'], infoDeserializada['ambiente'])
+			
+			# verificando se retornou informacoes do banco de dados		
+			if informacoesAcesso != None: 
+				# montando o contexto atual
+				contextoAtual = {}
+				contextoAtual['Data'] = time.strftime('%d/%m/%Y')
+				contextoAtual['Tempo'] = time.strftime('%H:%M')
+				contextoAtual['Recurso'] = infoDeserializada['contexto']
+				#print(infoDeserializada['contexto']['3'])
+				resultadoAvaliacao = self.avaliador.avaliar(contextoAtual,informacoesAcesso['contexto'])
+
+				if resultadoAvaliacao == True:
+					# registrando a sessao para o usuario no ambiente
+					if self.persistencia.criarSessao(informacoesAcesso) == False:
+						resultadoAvaliacao = False
+			else:
+				resultadoAvaliacao = False
+					
+			print("RESULTADO : " + str(resultadoAvaliacao))
+
+			# registrando evento
+			
+		else:
+			print("##### Usuário saindo no ambiente #####")
+			acao = "saindo"
+			# deletando sessao
+			if self.persistencia.deletarSessao(infoDeserializada['usuario'], infoDeserializada['ambiente']) == False:
+				resultadoAvaliacao = False
+			else:
+				resultadoAvaliacao = True
+
+			# registrando evento
+		
+		# enviando o resultado de volta para o ambiente
+		retorno['ambiente'] = infoDeserializada['ambiente']
+		retorno['acao'] = acao
+		retorno['resultado'] = resultadoAvaliacao
+		self.retornoAcesso(retorno)	 	
+
+	def retornoAcesso(self, info):		
+		self.gerenciador.enviarMensagem(info)
