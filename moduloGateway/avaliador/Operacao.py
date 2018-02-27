f2e9e54ef1096b9e82f38b39b47b33d63ccb46ef
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-27 02:07:31 -0300

    conserto de informacoes de log

--- moduloGateway/avaliador/Operacao.py
+++ moduloGateway/avaliador/Operacao.py
@@ -2,35 +2,35 @@
 class Operacao:
 
     def igual(self, valorRegra, valorAtual):
-        print("IGUAL")
+        print("Operação: IGUAL")
         if valorRegra == valorAtual:
             return True
         else:
             return False
 
     def diferente(self, valorRegra, valorAtual):
-        print("DIFERENTE")
+        print("Operação: DIFERENTE")
         if valorRegra != valorAtual:
             return True
         else:
             return False        
 
     def maior (self, valorRegra, valorAtual):
-        print("MAIOR")
+        print("Operação: MAIOR")
         if valorRegra > valorAtual:
             return True 
         else:
             return False
 
     def menor (self, valorRegra, valorAtual):
-        print("MENOR")
+        print("Operação: MENOR")
         if valorRegra < valorAtual:
             return True 
         else:
             return False      
 
     def between(self, valorRegra, valorAtual):
-        print("BETWEEN")
+        print("Operação: BETWEEN")
         if valorAtual >= valorRegra[0] and valorAtual <= valorRegra[1]:
             return True
         else:
