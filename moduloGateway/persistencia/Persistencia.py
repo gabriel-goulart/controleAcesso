e1440a8b9732db489a5d6aefcca9dc84bfff4327
gabriel-goulart <gabriel-r-g@hotmail.com>
2018-02-27 02:08:38 -0300

    adicionado funcoes para trabalhor com a sessao

--- moduloGateway/persistencia/Persistencia.py
+++ moduloGateway/persistencia/Persistencia.py
@@ -21,17 +21,63 @@ class Persistencia:
 
         query = "SELECT pa.id_papel_de_ambiente, pa.nome_papel_de_ambiente, up.id_papel_de_usuario, pu.nome_papel_de_usuario, ra.contexto FROM Regra_De_Acesso ra INNER JOIN Ambiente a ON a.id_ambiente = "+ambiente+" INNER JOIN Papel_De_Ambiente pa ON pa.id_papel_de_ambiente = a.id_papel_de_ambiente INNER JOIN Usuario_Papel up ON up.tag_usuario = \""+usuario+"\" AND up.id_papel_de_ambiente = pa.id_papel_de_ambiente INNER JOIN Papel_De_Usuario pu ON pu.id_papel_de_usuario = up.id_papel_de_usuario WHERE ra.id_papel_de_ambiente = pa.id_papel_de_ambiente AND ra.id_papel_de_usuario = pu.id_papel_de_usuario;"        
         try:
-            print("##### Buscando informações no banco de dados #####")            
+            print("##### Buscando informações da regra de acesso no banco de dados #####")            
             self.cursor.execute(query)
             result = self.cursor.fetchone()            
             if result == None :
-                retorno = {}
+                retorno = None
             else:
                 retorno['id_papel_de_ambiente']     = result[0]
                 retorno['nome_papel_de_ambiente']   = result[1]
                 retorno['id_papel_de_usuario']      = result[2]
                 retorno['nome_papel_de_usuario']    = result[3] 
-                retorno['contexto']                 = result[4]   
+                retorno['contexto']                 = result[4]
+                retorno['usuario']                  = usuario
+                retorno['ambiente']                 = ambiente   
         except:
-            retorno = {}
-        return retorno    
+            print("##### Buscando informações da regra de acesso no banco de dados - ERRO #####")
+            retorno = None
+        return retorno
+
+    def temSessao(self, usuario, ambiente):
+        retorno = False
+        query = "SELECT * FROM Sessao WHERE tag_usuario = \""+usuario+"\" AND id_ambiente = "+ambiente
+
+        try:
+            print("##### Buscando informações da sessão no banco de dados #####")            
+            self.cursor.execute(query)
+            result = self.cursor.fetchone()            
+            if result == None :
+                retorno = False
+            else:
+                retorno = True 
+        except:
+            print("##### Buscando informações da sessão no banco de dados  - ERRO #####")
+            retorno = False
+        return retorno
+
+    def criarSessao(self, info):
+        query = "INSERT INTO Sessao (tag_usuario, id_ambiente, id_papel_de_ambiente, id_papel_de_usuario, datetime)  VALUES ( \""+info['usuario']+"\", "+str(info['ambiente'])+", "+str(info['id_papel_de_ambiente'])+", "+str(info['id_papel_de_usuario'])+", NOW()) "
+
+        try:
+            print ("##### Registrando sessão para o usuário no ambiente #####")
+            self.cursor.execute(query)
+            self.bd.commit()
+            return True
+        except:
+            print ("##### Registrando sessão para o usuário no ambiente - ERRO #####")
+            self.bd.rollback()
+            return False
+
+    def deletarSessao(self, usuario, ambiente):
+        query = "DELETE FROM Sessao WHERE tag_usuario = \""+usuario+"\" AND id_ambiente = "+str(ambiente)
+
+        try:
+            print ("##### Deletando sessão do usuário no ambiente #####")
+            self.cursor.execute(query)
+            self.bd.commit()
+            return True
+        except:
+            print ("##### Deletando sessão do usuário no ambiente - ERRO #####")
+            self.bd.rollback()
+            return False
